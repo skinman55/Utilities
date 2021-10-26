b070e3240f62f503b7e216873c81c71e58fbc63f
Jeremy Skinner <jeremy@o3solution.com>
2021-10-26 14:52:10 -0500

    added branchamker

---
+++ BranchMaker/BranchMaker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BranchMaker
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var text = Clipboard.GetText();
+
+
+            Clipboard.SetText($"git fetch && git checkout {text}");
+        }
+    }
+}
